9b4e6bad3d4960768f5d2f25f88cb053403aeaff
unknown <bforte@.smoothstone.local>
2014-08-29 13:55:59 -0400

    scheduler in grid layout

--- GlobalDev/glib/global_components/schedule_maker.php
+++ GlobalDev/glib/global_components/schedule_maker.php
@@ -10,38 +10,34 @@ class Schedule_Maker {
 	public function make() {
 		$schedulerHTML = "";
 		$schedulerHTML .= "
-		
-
-<div class='container scheduler' >
-	<div class='schedulerRow schedulerTitle'>
-		Scheduler
-	</div>
+<div class='container scheduleMakerContainer'  >  ";
+	$schedulerHTML .= 'Scheduler:<button id="newFormToggle" onclick="toggleNewFormOn()">+</button><button id="newFormToggle" onclick="toggleNewFormOff()">-</button>';
+	$schedulerHTML .='<div id="newFormBody" style="display:none;">';
+	$schedulerHTML .= "
 	<form name='schedulerForm' method='get' action=" . $_SERVER['PHP_SELF'] . ">
 		<input type='hidden'  name='controller' value='".$this->controller."'/>
 		<input type='hidden'  name='method' value='addAppointment'/>
-		<input type='hidden'  name='user_id' value='".$this->user_id."'/>
-	<div class='schedulerRow'>
-		<div class='schedulerLabel' >title:</div>
-		<input id='title' class='schedulerInput' type='text' name='title' />
-	</div>
-	<div class='schedulerRow'>
-		<div class='schedulerLabel' >note:</div>
-		<textarea id='note'  class='schedulerInput schedulerTextarea'   name='note' ></textarea>
-	</div>
-	<div class='schedulerRow'>
-		<div class='schedulerLabel' >anchor:</div>
-		<input id='anchor'  class='schedulerInput' type='text' name='anchor' />
-	</div>
-	<div class='schedulerRow'>
-		<div class='schedulerLabel' >date:</div>
-		<input id='datepickScheduler'  class='schedulerInput' name='date' size='30' />
-	</div>
-	<div class='schedulerRow'>
-		<button    class='schedulerInput schedulerButton'  onClick='getDateSelected()'>
-			set
-		</button>
-	</div>
-	</form>
+		<input type='hidden'  name='user_id' value='".$this->user_id."'/>";
+		
+		$gridMatrix[0][0] = "title:";
+		$gridMatrix[0][1] = "<input id='title' class='iii' type='text' name='title' />";
+		$gridMatrix[1][0] = "note:";
+		$gridMatrix[1][1] = "<textarea id='note'  class='iii schedulerTextarea'   name='note' ></textarea>";
+		$gridMatrix[2][0] = "anchor:";
+		$gridMatrix[2][1] = "<input id='anchor'  class='iii' type='text' name='anchor' /> ";
+		$gridMatrix[3][0] = "date:";
+		$gridMatrix[3][1] = "<input id='datepickScheduler'  class='iii' name='date' size='30' />";
+		$gridMatrix[4][0] = "<button  class='iii bbb'  onClick='getDateSelected()'> set </button> ";
+		 
+		$rows = 5;
+		$cols = 2;
+		$columnWidthPercentsArray = array(40, 40);
+		$gridObject = new GridObject($gridMatrix, $columnWidthPercentsArray);
+		
+		$schedulerHTML .= $gridObject->make();
+		
+	$schedulerHTML .= "</form></div>";
+	$schedulerHTML .= "
 	  <script>
   $(function() {
     $( '#datepickScheduler' ).datepicker();
@@ -52,13 +48,20 @@ class Schedule_Maker {
 		function getDateSelected(){
 			 
 			document.schedulerForm.submit();
-		}
+		}function toggleNewFormOn(){
+				 
+				 document.getElementById('newFormBody').style.display = 'block';
+			}
+			function toggleNewFormOff(){
+				 
+				 document.getElementById('newFormBody').style.display = 'none';
+			}
 	</script>
 </div>";
 		
 		return $schedulerHTML;
 
 	}
-
+	
 }
 ?>
