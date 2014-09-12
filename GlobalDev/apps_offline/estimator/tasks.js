dd2c79ff5dffd5b923eaeed2597bace495493e58
unknown <bforte@.smoothstone.local>
2014-09-12 12:56:02 -0400

    tasks into table

--- GlobalDev/apps_offline/estimator/tasks.js
+++ GlobalDev/apps_offline/estimator/tasks.js
@@ -13,11 +13,54 @@ function add(o) {
   // add to store
   var request = store.add(o);
   request.onsuccess = function (e) {
-    console.log("Add 'person' successful! person=" + JSON.stringify(o));
+    console.log("Add 'task' successful! task=" + JSON.stringify(o));
    
   };
   request.onerror = function (e) {
     console.log("Add error", e.target.error.name);
   };
 }
- 
+//used to just be row
+function renderTask(key,value) {
+	 
+	  
+	return "<tr> <td>" +value["name"]+ "</td><td>" + value["email"] + " </td><td><a href='javascript:void(0);' style='color:red;'  onclick='deleteByKey(" + key + ")'>X</a></td></tr>";
+}
+ // find all
+function findAll() {
+  var tx = db.transaction(["tasks"], "readonly");
+  var objectStore = tx.objectStore("tasks");
+  var cursor = objectStore.openCursor();
+
+  cursor.onsuccess = function (e) {
+  	var rowOutput = "";
+	var taskList = document.getElementById("taskListBody");
+    var res = e.target.result;
+    if(!!res == false)
+      return;
+    
+      console.log("key", res.key);
+      console.log("value", res.value);
+      rowOutput += renderTask(res.key,res.value);
+      res.continue ();
+    
+      $('#taskList').append(rowOutput);
+  };
+}
+function deleteByKey(key){
+	alert('delete:'+key);
+}
+/*
+ * function loadTaskItems(tx, rs) {
+	var rowOutput = "<tr><th>Task</th><th title='Time to complete once.'>Minutes</th><th>Action</th></tr>";
+	var taskList = document.getElementById("taskList");
+	for (var i = 0; i < rs.rows.length; i++) {
+		rowOutput += renderTask(rs.rows.item(i));
+	}
+	taskList.innerHTML = rowOutput;
+}
+
+function renderTask(row) {
+	return "<tr> <td>" + row.task + "</td><td>" + row.minutes + " </td><td><a href='javascript:void(0);' style='color:red;'  onclick='taskNameSpace.webdb.deleteTask(" + row.ID + ");'>X</a></td></tr>";
+}
+ */
--- GlobalDev/apps_offline/estimator/tasks.php
+++ GlobalDev/apps_offline/estimator/tasks.php
@@ -3,12 +3,18 @@
 <body onload='openDB_global();' >
 	<div class="content">
 
-		<h3>Tasks</h3>
-		<div class='directions' style='font-size:11px;font-style:italic;'>
-			A general list of tasks.
-		</div>
-		  
-  <hr>
+		<h3>Task
+		<span class='directions' style='margin-left:20px;font-weight:normal;font-size:11px;font-style:italic;'>
+			A general list of tasks.</span>
+		</h3>
+		 
+		  <button id='newTaskFormButton' onclick='openNewTaskForm();' >
+				new task
+			</button>
+  		 <button id='TaskListButton' onclick='openTaskList();' >
+				 task list
+			</button>
+  <div id='newTaskForm' style='display:none;'>
 		<table border=1 class="estimatorTable">
 			<form type="post" onsubmit="addTask(); return false;">
 				<tr>
@@ -31,7 +37,35 @@
 		<table id="taskItems" border=1 class="estimatorTable"></table>
 
 	</div>
+	 
+	 <table id="taskList" border=1 class="estimatorTable">
+	  <thead><tr><th>Task</th><th title='Time to complete once.'>Minutes</th><th>Action</th></tr></thead>
+	 </table>
+  
+  </div>
+  
   <script src="tasks.js"></script>
   
 </body>
 </html>
+<script>
+$(document).ready(function() {
+		//var taskList = document.getElementById('taskList');
+		//taskList.style.display = "none";
+	});
+	function openNewTaskForm(){
+		var newTaskForm = document.getElementById('newTaskForm');
+		newTaskForm.style.display="block";
+		//var taskList = document.getElementById('taskList');
+		//taskList.style.display="none";
+	}
+	function openTaskList(){
+		//var taskList = document.getElementById('taskList');
+		//taskList.style.display="block";
+		var taskList = document.getElementById("taskList");
+		var newTaskForm = document.getElementById('newTaskForm');
+		newTaskForm.style.display="none";
+		 findAll();
+		//taskList.innerHTML = rowOutput;
+	}
+</script>
