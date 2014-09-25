29e049f68aef4030758a1d3e8d57a7777ab6fad1
Bayon Forte <bayon@forteworks.com>
2014-09-25 19:59:10 -0400

    remove previous offline work on apps

--- GlobalDev/apps_offline/estimator/background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.php', {
-    'bounds': {
-      'width': 600,
-      'height': 600
-    }
-  });
-});
--- GlobalDev/apps_offline/estimator/head/navigation.php
+++
@@ -1,6 +0,0 @@
-<div class='navigation_container'>
-	<a href="index.php" >home</a>
-	<a href="estimate.php" >estimate</a>
-	<a href="tasks.php" >tasks</a>
-	<a href="materials.php" >materials</a>
-</div>
